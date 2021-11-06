cbf539a54c745ada467095deead5a3be7314a040
CaroValdebenito <carolina.valdebenito@ug.uchile.cl>
2021-11-06 15:23:09 -0300

    Segundo Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,24 +13,7 @@ $(function () {
     //   $("p").hide();
     // });
   
-    $(".panqueques").click(function () {
-      $(".texto1").toggle("slow");
-    });
-  
-    $(".tiramisu").click(function () {
-      $(".texto2").toggle(1000);
-    });
-  
-    $(".plateada").click(function () {
-      $(".texto3").toggle(1000);
-    });
-  
-    $(".titulo1" ).on( "click", function() {
-      console.log( $( this ).css("color","red") );
-    });
-  
-    $(".titulo2" ).on( "click", function() {
-      console.log( $( this ).css("color","red") );
-    });
-  });
+    $('[data-toggle="tooltip"]').tooltip();
+
+    alert ("Bienvenidos")
   
